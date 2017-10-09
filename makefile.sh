b7401b43d1ff3e0e7d1816293d147e480c8eb3fe
OlgaKuratkina <olga.kuratkina@gmail.com>
2017-10-09 21:24:15 +0300

    updated makefile for mac

--- makefile.sh
+++ makefile.sh
@@ -1,4 +1,10 @@
-pip install virtualenv
+
+# for MAC and Unix
+pip3 install virtualenv
 virtualenv venv
-venv\Scripts\Activate
-pip install -r requirements.txt
+source ./venv/bin/activate
+pip3 install -r requirements.txt
+behave -v
+
+# venv\Scripts\Activate
+# pip3 install -r requirements.txt
